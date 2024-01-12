dfdd6863af9a2773f441ab6fc3341e6d55ab1915
Yakiv <yakiv.nevidomskiy@whykingz.com>
2024-01-12 22:51:38 +0200

    initial commit

---
+++ components/auth/login-btn.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+
+interface LoginBtnProps {
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
+}
+
+export const LoginBtn = ({ children, mode = "redirect", asChild }: LoginBtnProps) => {
+  const router = useRouter();
+  const handleLogin = () => {
+    router.push("/auth/login");
+  }
+
+  if (mode === "modal") {
+    return (
+     <span>
+       TODO: implement modal
+     </span>
+    )
+  }
+
+  return (
+    <span
+      onClick={handleLogin}
+      className="cursor-pointer"
+    >
+      {children}
+    </span>
+  )
+}
