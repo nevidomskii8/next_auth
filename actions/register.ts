dc8787c46481f88bb1b805d2425abf90f203b452
Yakiv <yakiv.nevidomskiy@whykingz.com>
2024-01-17 23:24:17 +0200

    db create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,11 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcryptjs"
+
 import { RegistrationSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import {getUserByEmail} from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegistrationSchema>) => {
   const validatedFields = RegistrationSchema.safeParse(values);
@@ -9,6 +13,27 @@ export const register = async (values: z.infer<typeof RegistrationSchema>) => {
   if (!validatedFields.success) {
     return { error: "Invalid fields!"};
   }
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) {
+    return { error: "User already in use!" };
+  }
+  debugger;
+  await db.user.create({
+    data: {
+      id: email,
+      password: hashedPassword,
+      name,
+    }
+  });
+
+
+  // TODO: Send email confirmation
+
 
   return { success: "Register Success!" };
 }
