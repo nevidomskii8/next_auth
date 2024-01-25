fb8855d86f1a14972fff91002e0e858d261b64b4
Yakiv <yakiv.nevidomskiy@whykingz.com>
2024-01-25 23:26:54 +0200

    play with callback

--- actions/login.ts
+++ actions/login.ts
@@ -30,5 +30,5 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     }
     throw error;
   }
-
+  return { success: "Login Success!" };
 }
--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import { db } from "@/lib/db";
 import authConfig  from "@/auth.config";
+import { getUserById } from "@/data/user";
 
 
 export const {
@@ -11,6 +12,33 @@ export const {
   signIn,
   signOut
 } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      console.log({SESSION_TOKEN: token});
+
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+
+    async jwt({token} ) {
+      if (!token?.sub) return token
+
+      const existingUser = await getUserById(token.sub)
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig
