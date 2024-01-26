9750ef148958b9f66deb5e653ac501918a982800
Yakiv <yakiv.nevidomskiy@whykingz.com>
2024-01-26 23:13:28 +0200

    extend type of user

---
+++ auth.d.ts
@@ -0,0 +1,21 @@
+import { UserRole } from "@prisma/client";
+import NextAuth, { type DefaultSession } from "next-auth";
+
+export type ExtendedUser = DefaultSession["user"] & {
+  role: "ADMIN" | "USER";
+};
+
+declare module "next-auth" {
+  interface Session {
+    user: UserRole;
+  }
+}
+
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    token?: string;
+    role?: "ADMIN" | "USER";
+  }
+}
